35c6425d4b7412e66ccdc77e5ef6af2cb2216a9c
Daniela Souza <danieladelfino042@gmail.com>
2021-02-14 23:59:57 -0300

    Tab Navigation - Ícones nas Tabs

--- react-native/navegacao/src/navegacao/index.js
+++ react-native/navegacao/src/navegacao/index.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
-import Drawer from './Drawer'
+/* import Drawer from './Drawer' */
 //import Stack from './Stack'
-// import Tab from './Tab'
+import Tab from './Tab'
 
 export default props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
-            <Drawer />
-            {/* <Tab /> */}
+            {/* <Drawer /> */}
+            <Tab />
             {/* <Stack /> */}
         </NavigationContainer>
     </SafeAreaView>
--- react-native/navegacao/src/views/TelaD.js
+++ react-native/navegacao/src/views/TelaD.js
@@ -5,10 +5,19 @@ import TextoCentral from '../components/TextoCentral'
 export default props => {
         return (
             <View style={{flex:1}}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
                     <Button 
                         title='Abrir' 
-                        onPress={() => props.navigation.navigate}/>
+                        onPress={() => {
+                            props.navigation.openDrawer()
+                            setTimeout(() => {
+                                props.navigation.closeDrawer()
+                                setInterval(() => {
+                                    props.navigation.toggleDrawer()
+                                }, 1000)
+                            }, 3000)
+                        }}
+                    />
                 </View>
                 <View style={{flex:1}}>
                     <TextoCentral corFundo='#FF9546' corTexto='#000'>
