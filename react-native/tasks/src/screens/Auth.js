e1a515602d1059f22f49e39b2fe99ef57797b469
Daniela Souza <danieladelfino042@gmail.com>
2021-02-25 00:47:09 -0300

    Task: Campos Login - Email e Password

--- react-native/tasks/src/screens/Auth.js
+++ react-native/tasks/src/screens/Auth.js
@@ -5,7 +5,8 @@ import {
     StyleSheet,
     View,
     TextInput, 
-    TouchableOpacity
+    TouchableOpacity,
+    Platform
 } from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
@@ -23,10 +24,18 @@ export default class Auth extends Component {
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
-                <View>
+                <View style={styles.formContainer}>
                     <TextInput placeholder='E-mail' value={this.state.email}
                         style={styles.input}
                         onChangeText={email => this.setState({ email })} />
+                    <TextInput placeholder='Senha' value={this.state.password}
+                        style={styles.input}
+                        onChangeText={password => this.setState({ password })} />
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Entrar</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </ImageBackground>
         )
@@ -47,6 +56,25 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     input: {
-
-    }
+        marginTop: 10,
+        backgroundColor: '#FFF',
+        padding: Platform.OS == 'ios' ? 15 : 10
+    },
+    formContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 20,
+        width: '90%',
+        
+    },
+    button: {
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+    },
 })
