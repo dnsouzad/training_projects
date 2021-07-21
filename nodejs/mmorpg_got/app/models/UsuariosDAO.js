ea86ba5c27085be672d1474c6dd6900057343a42
Daniela Souza <danieladelfino042@gmail.com>
2021-07-21 20:46:31 -0300

    Crypto com NodeJs

--- nodejs/mmorpg_got/app/models/UsuariosDAO.js
+++ nodejs/mmorpg_got/app/models/UsuariosDAO.js
@@ -1,13 +1,20 @@
+// importar o módulo do crypto
+
+var crypto = require("crypto")
+
 function UsuariosDAO(connection){
     this._connection = connection()
 }
 
-
-
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
     this._connection.open( function(err, mongoclient){
         mongoclient.collection("usuarios", function(err, collection){
-            collection.insert(usuario)
+            
+            var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex")
+
+            usuario.senha = senha_criptografada
+
+            collection.insert(usuario) 
             mongoclient.close()
         })
     } ) 
@@ -16,6 +23,10 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario){
 UsuariosDAO.prototype.autenticar = function(usuario, req, res){
     this._connection.open( function(err, mongoclient){
         mongoclient.collection("usuarios", function(err, collection){
+            
+            var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex")
+            usuario.senha = senha_criptografada
+            
             collection.find(usuario).toArray(function(err, result){
                 if(result[0] != undefined){
                     req.session.autorizado = true
